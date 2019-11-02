7d27c70b0a72c9750ce129f575aaee6a1ae40448
Christopher Aubut <christopher@aubut.me>
2019-11-01 19:13:37 -0600

    Add net-snmp dependency

--- build.py
+++ build.py
@@ -5,37 +5,78 @@ import re
 from typing import Any, MutableMapping
 
 from setuptools import Extension
+import os
+import re
+import sys
+import platform
+import subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
 NETSNMP_LIBS = os.popen('net-snmp-config --libs').read()
 
-EXTENSIONS = [
-    Extension(
-        'snmp_fetch.capi', [
-            'src/capi/capimodule.cpp',
-            'src/capi/session.cpp',
-            'src/capi/asyncio.cpp',
-            'src/capi/results.cpp',
-            'src/capi/types.cpp',
-            'src/capi/utils.cpp',
-            'src/capi/debug.cpp',
-        ],
-        depends=[
-            'src/capi/capimodule.hpp',
-            'src/capi/session.hpp',
-            'src/capi/asyncio.hpp',
-            'src/capi/results.hpp',
-            'src/capi/types.hpp',
-            'src/capi/utils.hpp',
-            'src/capi/debug.hpp',
-        ],
-        library_dirs=re.findall(r'-L(\S+)', NETSNMP_LIBS),
-        libraries=re.findall(r'-l(\S+)', NETSNMP_LIBS),
-        language='c++',
-        extra_compile_args=['-std=c++17']
-    ),
+# 
+# 
+# def build(setup_kwargs: MutableMapping[Any, Any]) -> None:
+#     """Entry point for build script."""
+#     setup_kwargs.update({'ext_modules': EXTENSIONS})
+# 
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+extensions = [
+    CMakeExtension('snmp_fetch.capi'),
 ]
 
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
 
-def build(setup_kwargs: MutableMapping[Any, Any]) -> None:
-    """Entry point for build script."""
-    setup_kwargs.update({'ext_modules': EXTENSIONS})
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+
+def build(setup_kwargs):
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    setup_kwargs.update(
+        {"ext_modules": extensions, "cmdclass": {"build_ext": CMakeBuild}}
+    )
